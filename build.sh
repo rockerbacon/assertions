2849f2ceea7809c4d5b20541d4e0817f67775f08
rockerbacon <vas.vitor.santos@gmail.com>
2019-10-18 04:24:16 -0300

    Cleaned scripts'

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 PROJECT_ROOT=$(realpath $(dirname $0))
-TESTS_SRC_DIR="$PROJECT_ROOT/tests"
-MAIN_SRC_DIR="$PROJECT_ROOT/src/main"
 BUILD_DIR="$PROJECT_ROOT/build"
-
-ESCAPED_PROJECT_ROOT=$(echo "$PROJECT_ROOT" | sed 's/\//\\\//g; s/\./\\\./g')
 
 mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
@@ -24,42 +20,33 @@ else
 fi
 ######### Commnd Line Interface #########
 
-determine_target_rule () {
-	TARGET_CLEAN_PATH=$(echo "$TARGET" | sed "s/${ESCAPED_PROJECT_ROOT}\///g; s/src\/main\///; s/.cpp$//")
-	TARGET_RULE=$(echo $TARGET_CLEAN_PATH | sed "s/\//_/g")
-	TARGET_EXISTS=$(make help | grep -o "$TARGET_RULE")
-	if [ "$TARGET_EXISTS" == "" ]; then
-		echo "Error: no target '$TARGET'"
-		exit 1
-	fi
-}
-
 if [ "$ACTION" == "clean" ]; then
 	rm -rf "$BUILD_DIR"
 elif [ "$ACTION" == "cmake" ]; then
 	cmake "$PROJECT_ROOT"
-	BUILD_STATUS=$?
 elif [ "$ACTION" == "all" ]; then
 	if [ ! -f "$BUILD_DIR/Makefile" ]; then
 		cmake "$PROJECT_ROOT"
 	fi
 	make
-	BUILD_STATUS=$?
 elif [ "$ACTION" == "target" ]; then
 	if [ ! -f "$BUILD_DIR/Makefile" ]; then
 		cmake "$PROJECT_ROOT"
 	fi
 	for TARGET in "${TARGETS[@]}"
 	do
-		determine_target_rule
-		make $TARGET_RULE
-		BUILD_STATUS=$?
+		export TARGET=$TARGET
+		source "$PROJECT_ROOT/.export_target_info.sh"
+		if [ $TARGET_BUILD_IS_OUTDATED ]; then
+			make $TARGET_RULE
+		fi
 	done
 else
 	echo "Error: unknown action $ACTION"
 	exit 1
 fi
 
+BUILD_STATUS=$?
 if [ ! "$BUILD_STATUS" -eq 0 ]; then
 	exit $BUILD_STATUS
 fi
--- run.sh
+++ run.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-PROJECT_ROOT=$(realpath $(dirname $0))
-TESTS_SRC_DIR="$PROJECT_ROOT/tests"
-MAIN_SRC_DIR="$PROJECT_ROOT/src/main"
-BUILD_DIR="$PROJECT_ROOT/build"
-TESTS_BUILD_DIR="$BUILD_DIR/tests"
-PROGRAM_BUILD_DIR="$BUILD_DIR/release/bin"
-
-ESCAPED_PROJECT_ROOT=$(echo "$PROJECT_ROOT" | sed 's/\//\\\//g; s/\./\\\./g')
+export PROJECT_ROOT=$(realpath $(dirname $0))
 
 ############# Command Line Interface ###############
 if [ "$#" -eq 0 ]; then
@@ -27,75 +20,25 @@ do
 done
 ############ Command Line Interface ###############
 
-determine_target_rule() {
-	TARGET_CLEAN_PATH=$(echo "$TARGET" | sed "s/${ESCAPED_PROJECT_ROOT}\///g; s/src\/main\///; s/.cpp$//")
-	TARGET_RULE=$(echo $TARGET_CLEAN_PATH | sed "s/\//_/g")
-}
+export TARGET=$TARGET
+source "${PROJECT_ROOT}/.export_target_info.sh"
 
-determine_target_source() {
-	TARGET_RELATIVE_PATH="$TARGET_CLEAN_PATH.cpp"
-	if [ $TARGET_IS_TEST ]; then
-		TARGET_SOURCE="$PROJECT_ROOT/$TARGET_RELATIVE_PATH"
-	else
-		TARGET_SOURCE="$MAIN_SRC_DIR/$TARGET_RELATIVE_PATH"
-	fi
-	if [ ! -f "$TARGET_SOURCE" ]; then
-		echo "Error: no such target"
-		exit 1
-	fi
-}
-
-determine_target_is_test () {
-	CONTAINS_TEST_PREFIX=$(echo $TARGET_RULE | grep -oe '^tests_')
-	if [ "$CONTAINS_TEST_PREFIX" != "" ]; then
-		TARGET_IS_TEST=true
-	fi
-}
-
-determine_target_executable() {
-	if [ $TARGET_IS_TEST ]; then
-		TARGET_EXECUTABLE="$TESTS_BUILD_DIR/$TARGET_RULE"
-	else
-		TARGET_EXECUTABLE="$PROGRAM_BUILD_DIR/$TARGET_RULE"
-	fi
-}
-
-determine_target_needs_rebuild() {
-	if [ ! -f "$TARGET_EXECUTABLE" ]; then
-		TARGET_NEEDS_REBUILD=true
-	else
-		TARGET_SOURCE_MODIFICATION_TIME=$(stat -c %Y "$TARGET_SOURCE")
-		TARGET_EXECUTABLE_MODIFICATION_TIME=$(stat -c %Y "$TARGET_EXECUTABLE")
-		if [ "$TARGET_SOURCE_MODIFICATION_TIME" -gt "$TARGET_EXECUTABLE_MODIFICATION_TIME" ]; then
-			TARGET_NEEDS_REBUILD=true
-		fi
-	fi
-}
-
-determine_target_rule
-determine_target_is_test
-determine_target_source
-determine_target_executable
-determine_target_needs_rebuild
-
-if [ $TARGET_NEEDS_REBUILD ]; then
-	./build.sh target "$TARGET"
-	BUILD_STATUS=$?
-	if [ ! "$BUILD_STATUS" -eq 0 ]; then
-		exit 1
-	fi
+./build.sh target "$TARGET"
+BUILD_STATUS=$?
+if [ ! "$BUILD_STATUS" -eq 0 ]; then
+	exit 1
 fi
 
-if [ "$TARGET_IS_TEST" ]; then
+if [ $TARGET_IS_TEST ]; then
 	if [ "$OUTPUT_MODE" == "stderr" ]; then
-		"$TARGET_EXECUTABLE" 1>/dev/null
+		"$TARGET_BINARY_PATH" 1>/dev/null
 	elif [ "$OUTPUT_MODE" == "full" ]; then
-		"$TARGET_EXECUTABLE"
+		"$TARGET_BINARY_PATH"
 	else
-		"$TARGET_EXECUTABLE" 2>/dev/null
+		"$TARGET_BINARY_PATH" 2>/dev/null
 	fi
 else
-	"$TARGET_EXECUTABLE"
+	"$TARGET_BINARY_PATH"
 fi
 
 TARGET_STATUS="$?"
