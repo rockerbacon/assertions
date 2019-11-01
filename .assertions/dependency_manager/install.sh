d8daad7ebdd040f5932786558c6e185bfa1f4718
rockerbacon <vas.vitor.santos@gmail.com>
2019-11-01 10:20:05 -0300

    Updated assertions-test to v1.0

--- .assertions/dependency_manager/install.sh
+++ .assertions/dependency_manager/install.sh
@@ -3,4 +3,4 @@ apt-package/install.sh g++
 apt-package/install.sh cmake
 apt-package/install.sh make
 systemlib/install.sh pthread
-git/install.sh https://github.com/rockerbacon/assertions-test.git b9e7fbdcd81dca10d0ac17b15fd20bcf4e302b8e
+git/install.sh https://github.com/rockerbacon/assertions-test.git v1.0
