985a99a73b0d2e5fc2882ac3fb18d24e0e25c9c3
rockerbacon <vas.vitor.santos@gmail.com>
2020-01-29 10:57:36 -0300

    Added setup and teardown scripts

---
+++ tests/setup_environment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+script_dir=$(realpath $(dirname $0))
+project_root=$(realpath "$script_dir/..")
+
+cd $project_root
+
+mkdir -p .local_tests
+environment_dir=$(mktemp -d .local_tests/XXXXXX)
+
+cp		*.sh 			"$environment_dir/"
+cp	-r	.assertions		"$environment_dir/"
+cp	-R	tests			"$environment_dir/"
+cp		CMakeLists.txt	"$environment_dir/"
+
+cd "$environment_dir"
+
